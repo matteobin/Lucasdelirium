7e6f7e241b9bb251ac756d60e7dcd5724442fb23
matteobin <matteobin@protonmail.com>
2019-05-16 14:53:04 +0200

    Added index for practical reasons.

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	$title = 'Sommario';
+	$description = 'Questa è una breve descrizione del contenuto della pagina per i motori di ricerca.';
+	require_once('head.php');
+?>
+<section class="row container">
+    <ul class="col-12">
+        <li class="col-12"><a href="article.php">Articolo</a></li>
+        <li class="col-12"><a href="hub.php">Hub</a></li>
+    </ul>
+</section>
+<script src="js/shared.js"></script>
+<?php require_once('footer.php'); ?>
