1c752cc6c8ca850cc72993a31936cbf79b577f6c
matteobin <matteobin@protonmail.com>
2019-05-17 14:41:04 +0200

    Added sizes back to figures.

--- hub.php
+++ hub.php
@@ -6,23 +6,40 @@
 <section class="container row">
     <h1 class="col-12 center">Hub</h1>
     <h2 class="col-12 center">Sezioni del sito</h2>
-    <div class="col-4">
-        <a href="#" class="col-12">
-            <h3 class="col-12 center">Sezione A</h3>
-            <p class="col-12 center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat est ac lorem scelerisque rutrum. Vestibulum diam est, condimentum vitae neque at, mattis scelerisque lacus.</p>
-        </a>
+    <div class="row">
+        <div class="col-4">
+            <a href="#" class="col-12">
+                <h3 class="col-12 center">Sezione A</h3>
+                <figure class="col-12 center"><img src="http://1.bp.blogspot.com/_r4rluMJnrjI/S8enLFMzEcI/AAAAAAAAACA/ukgFXweKZuc/s320/hamburger1.jpg" alt="" class="col-12 no-margin"></figure>
+                <p class="col-12 center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat est ac lorem scelerisque rutrum. Vestibulum diam est, condimentum vitae neque at, mattis scelerisque lacus.</p>
+            </a>
+        </div>
+        <div class="col-4">
+            <a href="#" class="col-12">
+                <h3 class="col-12 center">Sezione B</h3>
+                <p class="col-12 center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat est ac lorem scelerisque rutrum. Vestibulum diam est, condimentum vitae neque at, mattis scelerisque lacus.</p>
+            </a>
+        </div>
+        <div class="col-4">
+            <a href="#" class="col-12">
+                <h3 class="col-12 center">Sezione C</h3>
+                <p class="col-12 center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat est ac lorem scelerisque rutrum. Vestibulum diam est, condimentum vitae neque at, mattis scelerisque lacus.</p>
+            </a>
+        </div>
     </div>
-    <div class="col-4">
-        <a href="#" class="col-12">
-            <h3 class="col-12 center">Sezione B</h3>
-            <p class="col-12 center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat est ac lorem scelerisque rutrum. Vestibulum diam est, condimentum vitae neque at, mattis scelerisque lacus.</p>
-        </a>
-    </div>
-    <div class="col-4">
-        <a href="#" class="col-12">
-            <h3 class="col-12 center">Sezione C</h3>
-            <p class="col-12 center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat est ac lorem scelerisque rutrum. Vestibulum diam est, condimentum vitae neque at, mattis scelerisque lacus.</p>
-        </a>
+    <div class="row">
+        <div class="col-4">
+            <a href="#" class="col-12">
+                <h3 class="col-12 center">Sezione C</h3>
+                <p class="col-12 center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat est ac lorem scelerisque rutrum. Vestibulum diam est, condimentum vitae neque at, mattis scelerisque lacus.</p>
+            </a>
+        </div>
+        <div class="col-4">
+            <a href="#" class="col-12">
+                <h3 class="col-12 center">Sezione D</h3>
+                <p class="col-12 center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat est ac lorem scelerisque rutrum. Vestibulum diam est, condimentum vitae neque at, mattis scelerisque lacus.</p>
+            </a>
+        </div>
     </div>
 </section>
 <?php require_once('footer.php'); ?>
