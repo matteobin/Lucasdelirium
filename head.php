d4a7b9f96b51fa1030af3f2b5923588552b78767
matteobin <matteobin@protonmail.com>
2019-06-04 15:10:26 +0200

    Polish.

--- head.php
+++ head.php
@@ -1,21 +1,11 @@
 <?php
-    function sanitize_output($buffer) {
-        $search = array(
-            '/\>[^\S ]+/s',
-            '/[^\S ]+\</s',
-            '/(\s)+/s',
-            '/<!--(.|\s)*?-->/'
-        );
-        $replace = array(
-            '>',
-            '<',
-            '\\1',
-            ''
-        );
+    function sanitizeOutput($buffer) {
+        $search = array('/\>[^\S ]+/s', '/[^\S ]+\</s', '/(\s)+/s', '/<!--(.|\s)*?-->/');
+        $replace = array('>', '<', '\\1', '');
         $buffer = preg_replace($search, $replace, $buffer);
         return $buffer;
     }
-    ob_start("sanitize_output");
+    ob_start("sanitizeOutput");
 ?>
 <!doctype html>
 <html lang="it">
