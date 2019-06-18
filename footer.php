29e95b97243be991a3e299c0d0232fadead3e790
matteobin <matteobin@protonmail.com>
2019-06-18 14:29:12 +0200

    Fixed almost everything.

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
         </div>
         <footer class="row">
             <div class="container row no-margin disclaimer">
-                <h1 class="col-12">DISCLAIMER:</h1>
-                <p class="col-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu ipsum sit amet orci sagittis iaculis nec quis tortor. Phasellus non efficitur purus, et laoreet nulla. Sed vestibulum sit amet magna eu aliquet. Sed bibendum, nisl et tincidunt pharetra, nisl nisl consequat sem, at pulvinar metus magna non libero.</p>
+                <h1 class="col-12 title">DISCLAIMER:</h1>
+                <p class="col-12 small-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu ipsum sit amet orci sagittis iaculis nec quis tortor. Phasellus non efficitur purus, et laoreet nulla. Sed vestibulum sit amet magna eu aliquet. Sed bibendum, nisl et tincidunt pharetra, nisl nisl consequat sem, at pulvinar metus magna non libero.</p>
             </div>
             <div class="container row">
-				<ul class="social">
+				<ul class="socials-and-contacts">
 					<li><a href="#"><svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">Facebook<path d="m25 0a25 25 0 0 0-25 25 25 25 0 0 0 25 25 25 25 0 0 0 25-25 25 25 0 0 0-25-25zm3.6 10c2 0 3.6 0.1 4.1 0.2v4.8h-2.8c-2.2 0-2.6 1.1-2.6 2.6v3.7h5l-0.7 5.3h-4.3v13.4h-5.5v-13.4h-4.5v-5.3h4.5v-4.2c0-4.6 2.8-7 6.9-7z" fill="#dad4ca"/></svg></a></li>
 					<li><a href="#"><svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">Twitter<path d="m25 0a25 25 0 0 0-25 25 25 25 0 0 0 25 25 25 25 0 0 0 25-25 25 25 0 0 0-25-25zm5.8 12.8c1.8 0 3.4 0.7 4.5 1.9 1.4-0.3 2.7-0.8 3.9-1.5-0.5 1.4-1.4 2.6-2.7 3.4 1.2-0.1 2.4-0.5 3.5-1-0.8 1.2-1.9 2.3-3.1 3.2 0 0.3 0 0.5 0 0.8 0 8.1-6.2 17.5-17.5 17.5-3.5 0-6.7-1-9.4-2.8 0.5 0.1 1 0.1 1.5 0.1 2.9 0 5.5-1 7.6-2.6-2.7-0.1-5-1.8-5.7-4.3 0.4 0.1 0.8 0.1 1.2 0.1 0.6 0 1.1-0.1 1.6-0.2-2.8-0.6-4.9-3-4.9-6v-0.1c0.8 0.5 1.8 0.7 2.8 0.8-1.7-1.1-2.7-3-2.7-5.1 0-1.1 0.3-2.2 0.8-3.1 3 3.7 7.6 6.2 12.7 6.4-0.1-0.5-0.2-0.9-0.2-1.4 0-3.4 2.7-6.1 6.2-6.1z" fill="#dad4ca"/></svg></a></li>
 					<li><a href="#"><svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">RSS<path d="m25 0a25 25 0 0 0-25 25 25 25 0 0 0 25 25 25 25 0 0 0 25-25 25 25 0 0 0-25-25zm-11.6 12.5c13.1 0.5 23.6 11 24.1 24.1 0 0.5-0.4 0.9-0.9 0.9h-2.7c-0.5 0-0.9-0.4-0.9-0.9-0.4-10.7-9-19.2-19.7-19.7-0.5 0-0.9-0.4-0.9-0.9v-2.7c0-0.5 0.4-0.9 0.9-0.9zm0 8.1c8.6 0.5 15.5 7.4 16 16 0 0.5-0.4 0.9-0.9 0.9h-2.7c-0.5 0-0.9-0.4-0.9-0.8-0.4-6.2-5.4-11.2-11.7-11.7-0.5 0-0.8-0.4-0.8-0.9v-2.7c0-0.5 0.4-0.9 0.9-0.9zm2.6 9.8c2 0 3.6 1.6 3.6 3.6 0 2-1.6 3.6-3.6 3.6s-3.6-1.6-3.6-3.6c0-2 1.6-3.6 3.6-3.6z" fill="#dad4ca"/></svg></a></li>
@@ -19,7 +19,7 @@
             function checkWindowWidthToAddHeaderBanner() {
                 if (window.innerWidth>=720 && document.querySelector('header .banner')==null) {
                     window.removeEventListener('resize', checkWindowWidthToAddHeaderBanner);
-                    document.querySelector('header').insertAdjacentHTML('afterbegin', '<div class="row no-margin banner"><figure class="no-margin first"><img src="http://pizzahousenl.ca/wp-content/uploads/2016/04/pizza_trad_pepperoni.png" class="col-12 no-margin"></figure><h1 class="no-margin"><img src="https://images.ecosia.org/-JaM4_bmF9sLn_ASqpS0dB-Mv4c=/0x390/smart/https%3A%2F%2Fwww.tortealcioccolato.com%2Fwp-content%2Fuploads%2F2012%2F03%2F141119204.jpg" alt="Lucasdelirium" class="col-12 no-margin"></h1><figure class="no-margin"><img src="http://pizzahousenl.ca/wp-content/uploads/2016/04/pizza_trad_pepperoni.png" class="col-12 no-margin"></figure></div>');
+                    document.querySelector('header').insertAdjacentHTML('afterbegin', '<div class="banner row"><figure class="no-margin first-figure"><img src="http://pizzahousenl.ca/wp-content/uploads/2016/04/pizza_trad_pepperoni.png" class="col-12 no-margin"></figure><h1 class="no-margin logo"><img src="https://images.ecosia.org/-JaM4_bmF9sLn_ASqpS0dB-Mv4c=/0x390/smart/https%3A%2F%2Fwww.tortealcioccolato.com%2Fwp-content%2Fuploads%2F2012%2F03%2F141119204.jpg" alt="Lucasdelirium" class="col-12 no-margin"></h1><figure class="no-margin"><img src="http://pizzahousenl.ca/wp-content/uploads/2016/04/pizza_trad_pepperoni.png" class="col-12 no-margin"></figure></div>');
                 }
             }
             window.addEventListener('resize', checkWindowWidthToAddHeaderBanner);
--- js/generic-article.js
+++ js/generic-article.js
@@ -13,12 +13,12 @@ function removeClass(element, className) {
 		element.className = element.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
 	}
 }
-var lightboxElements = document.querySelectorAll('figure.lightbox a');
+var lightboxElements = document.querySelectorAll('.lightbox a');
 var lightboxElementsNumber = lightboxElements.length;
 if (lightboxElementsNumber!=0) {
-	document.body.insertAdjacentHTML('beforeend', '<div id="lightbox-background"></div><figure id="lightbox-viewer"><img class="loading" alt="Caricamento"><img class="loaded" alt=""><figcaption></figcaption></figure>');
-	var lightboxBackground = document.getElementById('lightbox-background');
-	var lightboxViewer = document.getElementById('lightbox-viewer');
+	document.body.insertAdjacentHTML('beforeend', '<div class="lightbox-background"></div><figure class="lightbox-viewer"><img class="loading" alt="Caricamento"><img class="loaded" alt=""><figcaption></figcaption></figure>');
+	var lightboxBackground = document.getElementsByClassName('lightbox-background')[0];
+	var lightboxViewer = document.getElementsByClassName('lightbox-viewer')[0];
 	function closeLightbox(event) {
 		var tagName = event.target.tagName.toLowerCase();
 		if (tagName!='img' && tagName!='figcaption') {
@@ -27,17 +27,17 @@ if (lightboxElementsNumber!=0) {
 			removeClass(lightboxBackground, 'visible');
 			removeClass(lightboxViewer, 'visible');
 			setTimeout(function() {
-				removeClass(lightboxBackground, 'active');
-				removeClass(lightboxViewer, 'active');
-				removeClass(lightboxViewer.children[0], 'visible');
-				removeClass(lightboxViewer.children[1], 'visible');
+				removeClass(lightboxBackground, 'render');
+				removeClass(lightboxViewer, 'render');
+				removeClass(lightboxViewer.children[0], 'render');
+				removeClass(lightboxViewer.children[1], 'render');
 			}, 334);
 		}
 	}
 	function showLoadedAndHideLoading() {
 			lightboxViewer.children[1].removeEventListener('load', showLoadedAndHideLoading);
-			removeClass(lightboxViewer.children[0], 'visible');
-			addClass(lightboxViewer.children[1], 'visible');
+			removeClass(lightboxViewer.children[0], 'render');
+			addClass(lightboxViewer.children[1], 'render');
 	}
 	function openLightbox(event) {
 		event.preventDefault();
@@ -47,10 +47,10 @@ if (lightboxElementsNumber!=0) {
 		if (typeof(event.target.parentElement.parentElement.children[1])!=='undefined') {
 			lightboxViewer.children[2].innerHTML = event.target.parentElement.parentElement.children[1].innerHTML;
 		}
-		addClass(lightboxViewer.children[0], 'visible');
-		addClass(lightboxBackground, 'active');
-		setTimeout(function() {addClass(lightboxBackground, 'visible');}, 34);
-		addClass(lightboxViewer, 'active');
+		addClass(lightboxViewer.children[0], 'render');
+		addClass(lightboxBackground, 'render');
+		setTimeout(function() {addClass(lightboxBackground, 'almost-visible');}, 34);
+		addClass(lightboxViewer, 'render');
 		setTimeout(function() {addClass(lightboxViewer, 'visible');}, 34);
 		lightboxViewer.children[1].addEventListener('load', showLoadedAndHideLoading);
 		lightboxBackground.addEventListener('click', closeLightbox);
@@ -61,7 +61,7 @@ if (lightboxElementsNumber!=0) {
 	}
 	delete(lightboxElementsIndex);
 }
-document.body.insertAdjacentHTML('beforeend', '<a href="#" id="go-up-button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 65 70" title="Clicca per tornare all\'inizio dell\'articolo.">Torna su<g fill="#19273d"><path d="M7.7 33.511L32.675 1.876 57.65 33.511z"/><g><path d="M20.376 47.078h-7.441V67.74H9.9V46.79H2.46V44.26h17.916zM41.182 56.976c0 2.24-.362 4.262-1.156 5.924s-1.879 2.962-3.251 3.9c-1.373.868-3.034 1.301-4.913 1.301-1.806 0-3.467-.433-4.84-1.373-1.445-.866-2.529-2.167-3.25-3.828-.796-1.662-1.157-3.613-1.23-5.78V55.46c0-2.24.362-4.19 1.157-5.924s1.878-3.034 3.25-3.901c1.446-.94 3.035-1.373 4.841-1.373 1.878 0 3.468.433 4.912 1.373 1.445.867 2.529 2.167 3.251 3.9.795 1.735 1.156 3.685 1.156 5.925v1.517zm-3.035-1.517c0-2.746-.578-4.84-1.661-6.285s-2.673-2.168-4.624-2.168-3.467.723-4.551 2.168-1.662 3.54-1.734 6.14v1.734c0 2.673.578 4.768 1.662 6.285s2.673 2.312 4.623 2.312 3.468-.723 4.552-2.167 1.661-3.468 1.661-6.141v-1.878zM48.84 58.71v9.03h-3.035V44.622h8.525c2.528 0 4.55.65 5.924 1.95 1.444 1.301 2.167 3.035 2.167 5.13 0 2.24-.723 3.973-2.095 5.201s-3.396 1.807-6.069 1.807zm0-2.529h5.49c1.661 0 2.89-.361 3.756-1.156s1.3-1.878 1.3-3.323c0-1.373-.433-2.456-1.3-3.323-.867-.795-2.095-1.228-3.612-1.3H48.84z"/></g></g></svg></a>');
+document.body.insertAdjacentHTML('beforeend', '<a href="#" class="go-up-button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 65 70" title="Clicca per tornare all\'inizio dell\'articolo.">Torna su<g fill="#19273d"><path d="M7.7 33.511L32.675 1.876 57.65 33.511z"/><g><path d="M20.376 47.078h-7.441V67.74H9.9V46.79H2.46V44.26h17.916zM41.182 56.976c0 2.24-.362 4.262-1.156 5.924s-1.879 2.962-3.251 3.9c-1.373.868-3.034 1.301-4.913 1.301-1.806 0-3.467-.433-4.84-1.373-1.445-.866-2.529-2.167-3.25-3.828-.796-1.662-1.157-3.613-1.23-5.78V55.46c0-2.24.362-4.19 1.157-5.924s1.878-3.034 3.25-3.901c1.446-.94 3.035-1.373 4.841-1.373 1.878 0 3.468.433 4.912 1.373 1.445.867 2.529 2.167 3.251 3.9.795 1.735 1.156 3.685 1.156 5.925v1.517zm-3.035-1.517c0-2.746-.578-4.84-1.661-6.285s-2.673-2.168-4.624-2.168-3.467.723-4.551 2.168-1.662 3.54-1.734 6.14v1.734c0 2.673.578 4.768 1.662 6.285s2.673 2.312 4.623 2.312 3.468-.723 4.552-2.167 1.661-3.468 1.661-6.141v-1.878zM48.84 58.71v9.03h-3.035V44.622h8.525c2.528 0 4.55.65 5.924 1.95 1.444 1.301 2.167 3.035 2.167 5.13 0 2.24-.723 3.973-2.095 5.201s-3.396 1.807-6.069 1.807zm0-2.529h5.49c1.661 0 2.89-.361 3.756-1.156s1.3-1.878 1.3-3.323c0-1.373-.433-2.456-1.3-3.323-.867-.795-2.095-1.228-3.612-1.3H48.84z"/></g></g></svg></a>');
 function updateDocumentHeight() {
 	documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
 }
@@ -79,16 +79,16 @@ function hasClass(element, className) {
 }
 var hideGoUpButtonTimeoutStarted = false;
 function hideGoUpButton() {
-	var goUpButton = document.getElementById('go-up-button');
+	var goUpButton = document.getElementsByClassName('go-up-button')[0];
 	hideGoUpButtonTimeoutStarted = true;
 	setTimeout(function() {removeClass(goUpButton, 'visible');}, 1834);
-	setTimeout(function() {removeClass(goUpButton, 'active');}, 2134);
+	setTimeout(function() {removeClass(goUpButton, 'render');}, 2134);
 }
 function checkScrolledToShowGoUpButton() {
-	var goUpButton = document.getElementById('go-up-button');
+	var goUpButton = document.getElementsByClassName('go-up-button')[0];
 	if (document.documentElement.scrollTop>=documentHeight/3) {
-		if (!hasClass(goUpButton, 'active')) {
-			addClass(goUpButton, 'active');
+		if (!hasClass(goUpButton, 'render')) {
+			addClass(goUpButton, 'render');
 			setTimeout(function() {addClass(goUpButton, 'visible');}, 34);
 			hideGoUpButtonTimeoutStarted = false;
 			if (window.innerWidth<720) {
@@ -99,7 +99,7 @@ function checkScrolledToShowGoUpButton() {
 		}
 	} else if (hasClass(goUpButton, 'visible')) {
 		removeClass(goUpButton, 'visible');
-		setTimeout(function() {removeClass(goUpButton, 'active');}, 334);
+		setTimeout(function() {removeClass(goUpButton, 'render');}, 334);
 	}
 }
 var checkScrolledToShowGoUpButtonTimeout = setTimeout(function() {checkScrolledToShowGoUpButton();}, 250);
