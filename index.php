f5e80a35324de7b67b6ee3a06a38484fe3e9880c
matteobin <matteobin@protonmail.com>
2019-12-06 20:06:23 +0100

    Fixed spaces.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-	$title = 'Sommario';
-	$description = 'Questa è una breve descrizione del contenuto della pagina per i motori di ricerca.';
-	require_once('head.php');
+    $title = 'Sommario';
+    $description = 'Questa è una breve descrizione del contenuto della pagina per i motori di ricerca.';
+    require_once('head.php');
 ?>
 <ul class="col-12">
     <li class="col-12"><a href="article.php">Articolo</a></li>
