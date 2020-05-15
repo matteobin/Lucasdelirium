3b39d1359dc3d19f5ccb003f74467e9e61646403
matteobin <matteobin@protonmail.com>
2020-05-15 13:31:53 +0200

    Latest Domenico's requests.

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
     $description = 'Questa è una breve descrizione del contenuto della pagina per i motori di ricerca.';
     require_once('head.php');
 ?>
-<ul class="col-12">
+<ol class="col-12">
     <li class="col-12"><a href="article.php">Articolo</a></li>
     <li class="col-12"><a href="hub.php">Hub</a></li>
-</ul>
+</ol>
 <?php require_once('footer.php'); ?>
